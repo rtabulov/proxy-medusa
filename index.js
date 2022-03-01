499c775a2ef2b7e422c4ee0c1a716efa4478101c
Rassul <rasul.tabulov@gmail.com>
2022-03-01 10:50:55 +0600

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// Create Express Server
+const app = express();
+
+// Configuration
+// const PORT = process.env.PORT || 3000;
+// const HOST = 'localhost';
+// const API_SERVICE_URL = 'https://meduza.io';
+const API_SERVICE_URL = 'https://jsonplaceholder.typicode.com';
+
+// Logging
+app.use(morgan('dev'));
+
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+  res.send(
+    'This is a proxy service which proxies to Billing and Account APIs.',
+  );
+});
+
+// Proxy endpoints
+app.use(
+  '/',
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+  }),
+);
+
+// Start the Proxy
+// app.listen(PORT, () => {
+//   console.log(`Starting Proxy at ${HOST}:${PORT}`);
+// });
+
+module.exports = app;
